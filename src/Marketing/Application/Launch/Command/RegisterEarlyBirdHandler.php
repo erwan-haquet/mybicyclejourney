e7c4da68ea1862efd79bcb96a3342df09cd15ff5
erwan <ehaquet.dev@gmail.com>
2022-05-26 11:33:23 +0200

    mailer: remove email test code

--- src/Marketing/Application/Launch/Command/RegisterEarlyBirdHandler.php
+++ src/Marketing/Application/Launch/Command/RegisterEarlyBirdHandler.php
@@ -5,22 +5,15 @@ namespace App\Marketing\Application\Launch\Command;
 use App\Marketing\Domain\Launch\Exception\EmailIsAlreadyRegistered;
 use App\Marketing\Domain\Launch\Model\EarlyBird;
 use App\Marketing\Domain\Launch\Repository\EarlyBirdRepositoryInterface;
-use App\Supporting\Domain\Email\MailerInterface;
-use App\Supporting\Domain\Email\Model\Email;
 use Library\CQRS\Command\CommandHandlerInterface;
 
 class RegisterEarlyBirdHandler implements CommandHandlerInterface
 {
     private EarlyBirdRepositoryInterface $repository;
-    private MailerInterface $mailer;
 
-    public function __construct(
-        EarlyBirdRepositoryInterface $repository,
-        MailerInterface              $mailer
-    )
+    public function __construct(EarlyBirdRepositoryInterface $repository,)
     {
         $this->repository = $repository;
-        $this->mailer = $mailer;
     }
 
     /**
@@ -34,13 +27,5 @@ class RegisterEarlyBirdHandler implements CommandHandlerInterface
         );
 
         $this->repository->add($earlyBird);
-
-        $email = new Email(
-            subject: 'Early bird registered',
-            to: $command->email,
-            text: 'Welcome on board !'
-        );
-
-        $this->mailer->send($email);
     }
 }
