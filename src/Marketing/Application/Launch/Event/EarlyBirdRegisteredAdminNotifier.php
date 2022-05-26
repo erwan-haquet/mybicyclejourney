b25c869190a6309df654887678579edab51a220f
erwan <ehaquet.dev@gmail.com>
2022-05-26 15:05:26 +0200

    notifier: use symfony admin recipient system

--- src/Marketing/Application/Launch/Event/EarlyBirdRegisteredAdminNotifier.php
+++ src/Marketing/Application/Launch/Event/EarlyBirdRegisteredAdminNotifier.php
@@ -8,23 +8,19 @@ use Library\Assert\Assert;
 use Library\CQRS\Event\EventHandlerInterface;
 use Symfony\Component\Notifier\Notification\Notification;
 use Symfony\Component\Notifier\NotifierInterface;
-use Symfony\Component\Notifier\Recipient\Recipient;
 
 class EarlyBirdRegisteredAdminNotifier implements EventHandlerInterface
 {
     private EarlyBirdRepositoryInterface $repository;
     private NotifierInterface $notifier;
-    private string $adminEmail;
 
     public function __construct(
         EarlyBirdRepositoryInterface $repository,
         NotifierInterface            $notifier,
-        string                       $adminEmail,
     )
     {
         $this->repository = $repository;
         $this->notifier = $notifier;
-        $this->adminEmail = $adminEmail;
     }
 
     public function __invoke(EarlyBirdRegistered $event): void
@@ -39,6 +35,8 @@ class EarlyBirdRegisteredAdminNotifier implements EventHandlerInterface
                 $earlyBird->getEmail()
             ));
 
-        $this->notifier->send($notification, new Recipient($this->adminEmail));
+        foreach ($this->notifier->getAdminRecipients() as $admin) {
+            $this->notifier->send($notification, $admin);
+        }
     }
 }
