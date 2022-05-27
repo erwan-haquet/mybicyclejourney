320702293fbfb61bcc1ff8dadd766ca899e062eb
erwan <ehaquet.dev@gmail.com>
2022-05-27 13:56:36 +0200

    styles: better sass architecture

--- webpack.config.js
+++ webpack.config.js
@@ -35,8 +35,6 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/app.js')
-    .addEntry('pages/homepage', './assets/pages/homepage.js')
-    .addEntry('pages/the-project', './assets/pages/the-project.js')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
