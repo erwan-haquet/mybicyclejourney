873e8cf1513e09c2b980881e24bd59977a04833c
Erwan Haquet <ehaquet.dev@gmail.com>
2022-06-22 13:52:07 +0200

    user: finalize signup / login / password reset process

--- src/Supporting/Infrastructure/Email/SymfonyMailer.php
+++ src/Supporting/Infrastructure/Email/SymfonyMailer.php
@@ -5,25 +5,26 @@ namespace App\Supporting\Infrastructure\Email;
 use App\Supporting\Domain\Email\MailerInterface;
 use App\Supporting\Domain\Email\Model\Email;
 use Symfony\Component\Mailer\MailerInterface as Mailer;
+use Symfony\Component\Mime\Address;
 use Symfony\Component\Mime\Crypto\DkimSigner;
 use Symfony\Component\Mime\Email as SymfonyEmail;
 
 class SymfonyMailer implements MailerInterface
 {
     private Mailer $mailer;
-    private string $sender;
+    private Address $sender;
     private ?DkimSigner $dkimSigner = null;
 
     public function __construct(
         Mailer $mailer,
-        string $sender,
+        string $senderName,
+        string $senderEmail,
         string $dkimPk = null,
         string $dkimDomain = null,
         string $dkimSelector = null
-    )
-    {
+    ) {
         $this->mailer = $mailer;
-        $this->sender = $sender;
+        $this->sender = Address::create(sprintf('%s <%s>', $senderName, $senderEmail));
 
         if (null !== $dkimPk) {
             $this->dkimSigner = new DkimSigner($dkimPk, $dkimDomain, $dkimSelector);
