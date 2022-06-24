e8901253c6b240517290a3800b23b00ca870e0b7
Erwan Haquet <ehaquet.dev@gmail.com>
2022-06-24 12:59:47 +0200

    sitemap: create a query and remove non indexable pages from it

---
+++ src/ContentManagement/Application/Website/Query/FindSitemap.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\ContentManagement\Application\Website\Query;
+
+use Library\CQRS\Query\Query;
+
+/**
+ * Finds pages to display in the sitemap.
+ */
+class FindSitemap extends Query
+{
+
+}
--- src/ContentManagement/Ui/Website/Web/Controller/SitemapController.php
+++ src/ContentManagement/Ui/Website/Web/Controller/SitemapController.php
@@ -2,8 +2,9 @@
 
 namespace App\ContentManagement\Ui\Website\Web\Controller;
 
-use App\ContentManagement\Domain\Website\Repository\PageRepositoryInterface;
+use App\ContentManagement\Application\Website\Query\FindSitemap;
 use App\ContentManagement\Ui\Website\Web\Dto\Sitemap\SitemapDto;
+use Library\CQRS\Query\QueryBus;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,12 +12,15 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/sitemap.xml', name: 'sitemap', defaults: ['_format' => 'xml'])]
 class SitemapController extends AbstractController
 {
-    public function __invoke(PageRepositoryInterface $pageRepository): Response
+    public function __invoke(
+        QueryBus $queryBus
+    ): Response
     {
-        $pages = $pageRepository->findAll();
+        $query = new FindSitemap();
+        $sitemap = $queryBus->query($query);
         
         return $this->render('web/static_pages/sitemap.html.twig', [
-            'sitemap' => SitemapDto::new($pages)
+            'sitemap' => $sitemap
         ]);
     }
 }
