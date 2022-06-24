a2f396914b20c1dbc4229eb30313a8d5bdcbaed4
Erwan Haquet <ehaquet.dev@gmail.com>
2022-06-24 13:05:07 +0200

    theming: form control outline primary

--- src/ContentManagement/Ui/Website/Web/Controller/MetadataController.php
+++ src/ContentManagement/Ui/Website/Web/Controller/MetadataController.php
@@ -44,6 +44,4 @@ class MetadataController extends AbstractController
             'metadata' => $metadata
         ]);
     }
-
-
 }
--- src/ContentManagement/Ui/Website/Web/Controller/SitemapController.php
+++ src/ContentManagement/Ui/Website/Web/Controller/SitemapController.php
@@ -3,7 +3,6 @@
 namespace App\ContentManagement\Ui\Website\Web\Controller;
 
 use App\ContentManagement\Application\Website\Query\FindSitemap;
-use App\ContentManagement\Ui\Website\Web\Dto\Sitemap\SitemapDto;
 use Library\CQRS\Query\QueryBus;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,13 +11,11 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/sitemap.xml', name: 'sitemap', defaults: ['_format' => 'xml'])]
 class SitemapController extends AbstractController
 {
-    public function __invoke(
-        QueryBus $queryBus
-    ): Response
+    public function __invoke(QueryBus $queryBus): Response
     {
         $query = new FindSitemap();
         $sitemap = $queryBus->query($query);
-        
+
         return $this->render('web/static_pages/sitemap.html.twig', [
             'sitemap' => $sitemap
         ]);
