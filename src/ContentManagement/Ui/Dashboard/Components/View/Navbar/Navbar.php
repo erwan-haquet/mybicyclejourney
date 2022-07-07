d3de2a3157f3fdd48ee1ef01f011d966c17f0345
erwan <ehaquet.dev@gmail.com>
2022-07-07 19:48:40 +0200

    dashboard: create navigation for dashboard

---
+++ src/ContentManagement/Ui/Dashboard/Components/View/Navbar/Navbar.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\ContentManagement\Ui\Dashboard\Components\View\Navbar;
+
+use Library\Utils\View;
+
+class Navbar extends View
+{
+    public LocaleSwitcher $localeSwitcher;
+}
